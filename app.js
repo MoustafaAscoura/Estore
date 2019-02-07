28636fca6d2b680185acc42499e4d628f7e199bc
Moustafa Ascoura <askoura2008@yahoo.com>
2019-02-07 09:47:49 +0200

    Validation

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
-
+var validator = require('express-validator');
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/shopping', {useNewUrlParser: true } );
@@ -21,6 +21,7 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(flash());
